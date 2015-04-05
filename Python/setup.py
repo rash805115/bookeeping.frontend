4e17fe31c8a45ab8df7ade7be94e5237de991898
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-04-05 02:12:24 -0400

    Python - setup script update
    setup scripts indicates that before installation of this API, python
    needs to fetch the given packages.

--- Python/setup.py
+++ Python/setup.py
@@ -6,5 +6,6 @@ setuptools.setup(
         url = "https://github.com/rash805115/bookeeping.frontend/tree/master/Python",
         author = "Rahul Chaudhary",
         author_email = "rahul300chaudhary400@gmail.com",
-        description = "Python flavored API to assist developing the frontend for BooKeeping project."
+        description = "Python flavored API to assist developing the frontend for BooKeeping project.",
+        install_requires = ["boto==2.37.0"]
 )
