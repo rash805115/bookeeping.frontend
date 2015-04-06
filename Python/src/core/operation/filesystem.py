d90310f3229771fe5d7b1d5cf5f7b1b8eae01e31
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-04-06 19:52:29 -0400

    Python - Filesystem functions update

--- Python/src/core/operation/filesystem.py
+++ Python/src/core/operation/filesystem.py
@@ -15,3 +15,23 @@ class Filesystem:
 			"filesystemId": filesystem_id
 		}
 		commit.payload.update({"Filesystem_Create": sub_payload})
+	
+	def create_filesystem_version(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Version": sub_payload})
+	
+	def delete_filesystem(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Delete": sub_payload})
+	
+	def restore_filesystem(self, commit, user_id, filesystem_id, node_id_to_be_restored):
+		sub_payload = {
+			"userId": user_id,
+			"filesystemId": filesystem_id,
+			"nodeIdToBeRestored": node_id_to_be_restored
+		}
+		commit.payload.update({"Filesystem_Restore": sub_payload})
