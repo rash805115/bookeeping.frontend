11dcd5d0a1f950fcf096cc070e271238c29ca866
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-04-07 00:16:10 -0400

    Python - File & Directory operations.

--- Python/src/core/operation/directory.py
+++ Python/src/core/operation/directory.py
@@ -21,3 +21,38 @@ class Directory:
 			"directoryName": directory_name
 		}
 		commit.payload.update({"Directory_Create": sub_payload})
+	
+	def create_directory_version(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Version": sub_payload})
+	
+	def delete_directory(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Delete": sub_payload})
+	
+	def restore_directory(self, commit, user_id, filesystem_id, filesystem_version, directory_path, directory_name, node_id_to_be_restored):
+		sub_payload = {
+			"userId": user_id,
+			"filesystemId": filesystem_id,
+			"filesystemVersion": filesystem_version,
+			"directoryPath": directory_path,
+			"directoryName": directory_name,
+			"nodeIdToBeRestored": node_id_to_be_restored
+		}
+		commit.payload.update({"Directory_Restore": sub_payload})
+	
+	def move_directory(self, commit, user_id, filesystem_id, filesystem_version, old_directory_path, old_directory_name, new_directory_path, new_directory_name):
+		sub_payload = {
+			"userId": user_id,
+			"filesystemId": filesystem_id,
+			"filesystemVersion": filesystem_version,
+			"oldDirectoryPath": old_directory_path,
+			"oldDirectoryName": old_directory_name,
+			"newDirectoryPath": new_directory_path,
+			"newDirectoryName": new_directory_name
+		}
+		commit.payload.update({"Directory_Move": sub_payload})
--- Python/src/core/operation/file.py
+++ Python/src/core/operation/file.py
@@ -21,3 +21,38 @@ class File:
 			"fileName": file_name
 		}
 		commit.payload.update({"File_Create": sub_payload})
+	
+	def create_file_version(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Version": sub_payload})
+	
+	def delete_file(self, commit, node_id):
+		sub_payload = {
+			"nodeId": node_id
+		}
+		commit.payload.update({"Node_Delete": sub_payload})
+	
+	def restore_file(self, commit, user_id, filesystem_id, filesystem_version, file_path, file_name, node_id_to_be_restored):
+		sub_payload = {
+			"userId": user_id,
+			"filesystemId": filesystem_id,
+			"filesystemVersion": filesystem_version,
+			"filePath": file_path,
+			"fileName": file_name,
+			"nodeIdToBeRestored": node_id_to_be_restored
+		}
+		commit.payload.update({"File_Restore": sub_payload})
+	
+	def move_file(self, commit, user_id, filesystem_id, filesystem_version, old_file_path, old_file_name, new_file_path, new_file_name):
+		sub_payload = {
+			"userId": user_id,
+			"filesystemId": filesystem_id,
+			"filesystemVersion": filesystem_version,
+			"oldFilePath": old_file_path,
+			"oldFileName": old_file_name,
+			"newFilePath": new_file_path,
+			"newFileName": new_file_name
+		}
+		commit.payload.update({"File_Move": sub_payload})
