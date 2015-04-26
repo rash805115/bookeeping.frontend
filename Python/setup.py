bc5ec302b8f41cff145fe32fed2c2acf6e3e2e5a
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-04-26 11:30:00 -0400

    Python - Src packaged renamed to "pybookeeping" setup.py updated
    The src packaged was renamed because this is an installable module, and
    with the src package, the name of the package inside site-packages after
    installation was called "src" which is misleading.
    
    Giving a name to this project makes it pip-8 compliant and also gives a
    name under site-packages
    
    setup.py was updated and installed on the developer machine.

--- Python/setup.py
+++ Python/setup.py
@@ -1,11 +1,12 @@
 import setuptools
 
 setuptools.setup(
-        name = "Python.BooKeeping.Frontend",
+        name = "pybookeeping",
         version = 0.0,
         url = "https://github.com/rash805115/bookeeping.frontend/tree/master/Python",
         author = "Rahul Chaudhary",
         author_email = "rahul300chaudhary400@gmail.com",
         description = "Python flavored API to assist developing the frontend for BooKeeping project.",
-        install_requires = ["boto == 2.37.0", "requests == 2.6.0"]
+        install_requires = ["boto == 2.37.0", "requests == 2.6.0"],
+        packages = setuptools.find_packages()
 )
